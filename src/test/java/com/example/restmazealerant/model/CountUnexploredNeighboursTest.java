dba002d351045d3ff514717ff389d110736ce5d8
Milan Andras Gyuricska <vapitea@gmail.com>
2020-09-01 20:55:49 +0200

    Unit tests for the Cell class

---
+++ src/test/java/com/example/restmazealerant/model/CountUnexploredNeighboursTest.java
@@ -0,0 +1,38 @@
+package com.example.restmazealerant.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CountUnexploredNeighboursTest {
+
+    @Test
+    void countUnexploredNeighboursZero() {
+        //Arrange
+        Cell cell = new Cell();
+        cell.setRouteToNeighbours(new Cell.RouteType[]{Cell.RouteType.VISITED, Cell.RouteType.BLOCKED, null, null});
+
+        //Act
+        int unexpolerNeighbours = cell.countUnexploredNeighbours();
+
+        //Assert
+        assertEquals(0, unexpolerNeighbours);
+    }
+
+    @Test
+    void countUnexploredNeighboursTwo() {
+        //Arrange
+        Cell cell = new Cell();
+        cell.setRouteToNeighbours(new Cell.RouteType[]{Cell.RouteType.VISITED, Cell.RouteType.UNEXPLORED, null, Cell.RouteType.UNEXPLORED});
+
+        //Act
+        int unexpolerNeighbours = cell.countUnexploredNeighbours();
+
+        //Assert
+        assertEquals(2, unexpolerNeighbours);
+    }
+
+
+}
